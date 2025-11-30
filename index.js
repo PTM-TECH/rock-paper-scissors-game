e9721763025175f81dc611e96a9aa987193079d7
Patrick Mutua <pawatechsys@gmail.com>
2025-11-30 14:01:13 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+
+
+choices =["rock", "paper", "scissors"];
+const playerResult = document.getElementById("playerResult");
+const computerResult = document.getElementById("computerResult");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
+
+function playGame(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result;
+    
+    if(playerChoice===computerChoice){
+
+        result = "IT'S A DRAW!";
+
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                result = (computerChoice=== "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice=== "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (computerChoice=== "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
+    }
+    playerResult.textContent = `PLAYER: ${playerChoice}`;
+    computerResult.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("greenColor", "redColor");
+
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenColor");
+            playerScore ++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redColor");
+            computerScore ++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
+
+    }
+
+}
+
+
+
+
